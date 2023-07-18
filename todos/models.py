1050766ff4cee4e7870f65406e6028765f79eb67
Peter <peterkim647@gmail.com>
2023-07-18 11:25:51 -0700

    Feature 5 Complete

--- todos/models.py
+++ todos/models.py
@@ -5,3 +5,15 @@ from django.conf import settings
 class TodoList(models.Model):
     name = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True)
+
+
+class TodoItem(models.Model):
+    task = models.CharField(max_length=100)
+    due_date = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    list = models.ForeignKey(
+        TodoList,
+        related_name="items",
+        on_delete=models.CASCADE,
+        null=True,
+    )
