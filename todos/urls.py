9cbcbb5f16413dcd9983c65653642a7616b01e05
Peter <peterkim647@gmail.com>
2023-07-18 18:01:28 -0700

    Final Product

--- todos/urls.py
+++ todos/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("create/", todo_list_create, name="todo_list_create"),
     path("<int:id>/edit/", todo_list_update, name="todo_list_update"),
     path("<int:id>/delete/", todo_list_delete, name="todo_list_delete"),
-    path("/items/create/", todo_item_create, name="todo_item_create"),
+    path("items/create/", todo_item_create, name="todo_item_create"),
     path("items/<int:id>/edit/", todo_item_update, name="todo_item_update"),
 ]
